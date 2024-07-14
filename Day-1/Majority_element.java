c32ece25ad5cc46efff650d9c3e8501d2127fe2d
Akshaycodesig <147849204+Akshaycodesig@users.noreply.github.com>
2024-07-14 22:56:29 +0530

    Update Majority_element.java

--- Day-1/Majority_element.java
+++ Day-1/Majority_element.java
@@ -7,3 +7,34 @@ class Solution {
 
     }
 }
+
+// Using HashMap
+import java.util.*;
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!map.containsKey(nums[i]))
+            {
+                map.put(nums[i],1);
+            }
+            else
+            {   
+                int temp = map.get(nums[i]) + 1;
+                map.put(nums[i],temp);
+            }
+        }
+
+        int max = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.get(nums[i]) > (int)nums.length/2)
+                max = nums[i];
+        }
+
+        return max;
+    }
+}
